987ceccc79ea7068880c70304b3abe6a32c1192d
Taite Jernigan <taite5100@gmail.com>
2021-10-18 11:09:44 -0700

    Created the basic HTML layout and added click events to add an 'X'

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,10 @@
+console.log('hello')
 
 
+var squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+  square.addEventListener('click', event => {
+  square.textContent = 'X'
+  });
+})
